25720603a60373de41d59974a5617373b482e2f9
tibaredha <tibaredha@yahoo.fr>
2018-12-22 09:35:26 +0000

    initialisation

--- views/index/index.php
+++ views/index/index.php
@@ -26,6 +26,22 @@
     </div>	
 -->
 <?php 
+// echo 'max_execution_time = ' . ini_get('max_execution_time') . "\n";
+// ini_set("max_execution_time", "10") . "\n";
+// echo 'max_execution_time = ' . ini_get('max_execution_time') . "\n";
+// ini_restore ( "max_execution_time" );
+// echo 'max_execution_time = ' . ini_get('max_execution_time') . "\n";
+
+// print_r(ini_get_all());
+
+// echo 'display_errors = ' . ini_get('display_errors') . "\n";
+// echo 'register_globals = ' . ini_get('register_globals') . "\n";
+// echo 'post_max_size = ' . ini_get('post_max_size') . "\n";
+// echo 'post_max_size+1 = ' . (ini_get('post_max_size')+1) . "\n";
+// echo 'post_max_size in bytes = ' . return_bytes(ini_get('post_max_size'));
+// if(! ini_set("max_execution_time", "10")) {echo "échec";}else {echo "ok";}
+
+
 
 // function uncompress($srcName, $dstName) {
 // $string = implode("", gzfile($srcName));
